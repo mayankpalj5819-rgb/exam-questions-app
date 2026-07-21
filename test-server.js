4410e68c6d691767e3aceb474513103a031a730d
Z User <z@container>
2026-07-21 15:35:49 +0000

    cleanup: remove debug files, revert error detail exposure

--- test-server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`OK - Node ${process.version} - PORT ${process.env.PORT} - CWD ${process.cwd()}`);
-});
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Test server running on port ${process.env.PORT || 3000}`);
-});
